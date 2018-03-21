464748b56cec8862335f7bffaf5682bb772c9203
Nathan <nathan.sart@outlook.fr>
2018-03-21 14:22:14 +0100

    Modifs GUI

--- frame.cpp
+++ frame.cpp
@@ -17,31 +17,31 @@ Frame::Frame(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Frame) {
     ui->setupUi(this);
+
+    QString result;
     QIcon icon1, icon2, icon3, icon4, icon5, icon6, fleche1, fleche2, tel;
     fleche1.addFile(QStringLiteral("img/ihm/f.png"), QSize(), QIcon::Normal, QIcon::Off);
     fleche2.addFile(QStringLiteral("img/ihm/f2.png"), QSize(), QIcon::Normal, QIcon::Off);
     tel.addFile(QStringLiteral("img/ihm/phone.png"), QSize(), QIcon::Normal, QIcon::Off);
-    icon1.addFile(QStringLiteral("img/new/3.jpg"), QSize(), QIcon::Normal, QIcon::Off);
-    icon2.addFile(QStringLiteral("img/new/2.jpg"), QSize(), QIcon::Normal, QIcon::Off);
-    icon3.addFile(QStringLiteral("img/new/1.jpg"), QSize(), QIcon::Normal, QIcon::Off);
-    icon4.addFile(QStringLiteral("img/new/missk8.jpg"), QSize(), QIcon::Normal, QIcon::Off);
-    icon5.addFile(QStringLiteral("img/new/nathan.jpg"), QSize(), QIcon::Normal, QIcon::Off);
-    icon6.addFile(QStringLiteral("img/new/titouan.jpg"), QSize(), QIcon::Normal, QIcon::Off);
-    ui->precedent->setIcon(fleche1);
-    ui->suivant->setIcon(fleche2);
-    ui->telephoner->setIcon(tel);
-    ui->contact1->setIcon(icon1);
-    ui->contact2->setIcon(icon2);
-    ui->contact3->setIcon(icon3);
-    ui->contact4->setIcon(icon4);
-    ui->contact5->setIcon(icon5);
-    ui->contact6->setIcon(icon6);
-    ui->contact1->setIconSize(QSize(128, 128));
-    ui->contact2->setIconSize(QSize(128, 128));
-    ui->contact3->setIconSize(QSize(128, 128));
-    ui->contact4->setIconSize(QSize(128, 128));
-    ui->contact5->setIconSize(QSize(128, 128));
-    ui->contact6->setIconSize(QSize(128, 128));
+    result = this->query("\"SELECT photo FROM contacts WHERE prenom = 'Louis'\"");
+    icon1.addFile(result, QSize(), QIcon::Normal, QIcon::Off);
+    result = this->query("\"SELECT photo FROM contacts WHERE prenom = 'Leo'\"");
+    icon2.addFile(result, QSize(), QIcon::Normal, QIcon::Off);
+    result = this->query("\"SELECT photo FROM contacts WHERE prenom = 'Adrien'\"");
+    icon3.addFile(result, QSize(), QIcon::Normal, QIcon::Off);
+    result = this->query("\"SELECT photo FROM contacts WHERE prenom = 'Kateryna'\"");
+    icon4.addFile(result, QSize(), QIcon::Normal, QIcon::Off);
+    result = this->query("\"SELECT photo FROM contacts WHERE prenom = 'Nathan'\"");
+    icon5.addFile(result, QSize(), QIcon::Normal, QIcon::Off);
+    result = this->query("\"SELECT photo FROM contacts WHERE prenom = 'Titouan'\"");
+    icon6.addFile(result, QSize(), QIcon::Normal, QIcon::Off);
+
+    ui->precedent->setIcon(fleche1); ui->suivant->setIcon(fleche2); ui->telephoner->setIcon(tel);
+    ui->contact1->setIcon(icon1); ui->contact2->setIcon(icon2); ui->contact3->setIcon(icon3);
+    ui->contact4->setIcon(icon4); ui->contact5->setIcon(icon5); ui->contact6->setIcon(icon6);
+    ui->contact1->setIconSize(QSize(128, 128)); ui->contact2->setIconSize(QSize(128, 128));
+    ui->contact3->setIconSize(QSize(128, 128)); ui->contact4->setIconSize(QSize(128, 128));
+    ui->contact5->setIconSize(QSize(128, 128)); ui->contact6->setIconSize(QSize(128, 128));
 }
 
 Frame::~Frame()
@@ -57,6 +57,7 @@ QString Frame::query(QString command) {
     process.waitForFinished(-1);
     out = process.readAllStandardOutput();
     out.toStdString();
+    out.remove(out.length() - 1, 1);
     return out;
 }
 
@@ -68,14 +69,13 @@ void Frame::on_suivant_clicked()
 void Frame::on_contact1_clicked()
 {
     QString result;
-    result = this->query("\"SELECT numero FROM contacts WHERE prenom = 'Louis'\"");
+    result = this->query("\"SELECT telephone FROM contacts WHERE prenom = 'Louis'\"");
     qDebug() << result;
 
 }
 
 void Frame::on_contact2_clicked()
 {
-    int r = system("whoami");
-    std::cout << r;
+
 }
 
